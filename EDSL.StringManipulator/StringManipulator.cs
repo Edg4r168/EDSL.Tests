adeb49fcf0d91203beaf5b7153fde1ffaf538d9e
edgar sibrian <edg4r168@gmail.com>
2024-10-10 15:13:15 -0600

    Implementar metodos para manipular string

---
+++ EDSL.StringManipulator/StringManipulator.cs
@@ -0,0 +1,18 @@
+﻿namespace EDSL.StringManipulator;
+
+public class StringManipulator
+{
+    public string ReverseString(string input)
+    {
+        char[] chars = input.ToCharArray();
+
+        Array.Reverse(chars);
+
+        return new string(chars);
+    }
+
+    public string RemoveSpaces(string input)
+    {
+        return input.Replace(" ", "");
+    }
+}
