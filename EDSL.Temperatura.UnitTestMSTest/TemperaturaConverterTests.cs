1536292525b3211863abfe81518a909724d84a3f
edgar sibrian <edg4r168@gmail.com>
2024-10-10 15:18:17 -0600

    Crear tests para los metodos de TemperaturaConverter

---
+++ EDSL.Temperatura.UnitTestMSTest/TemperaturaConverterTests.cs
@@ -0,0 +1,23 @@
+﻿namespace EDSL.Temperatura.Tests;
+
+[TestClass()]
+public class TemperaturaConverterTests
+{
+    private readonly TemperaturaConverter _converter;
+
+    [TestMethod()]
+    public void CelsiusToFahrenheitTest()
+    {
+        double result = _converter.CelsiusToFahrenheit(25);
+
+        Assert.AreEqual(77, result);
+    }
+
+    [TestMethod()]
+    public void FahrenheitToCelsiusTest()
+    {
+        double result = _converter.CelsiusToFahrenheit(77);
+
+        Assert.AreEqual(25, result);
+    }
+}
