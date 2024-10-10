c47d3705ba8f329a0a96bf8aff41ef879f37b29b
edgar sibrian <edg4r168@gmail.com>
2024-10-10 15:18:49 -0600

    first commit

---
+++ Test.Calculadora.UnitTest.AppMSTest/CalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Calculadora.Tests;
+
+[TestClass()]
+public class CalculatorTests
+{
+    private readonly Calculator _calculator = new Calculator();
+
+    [TestMethod()]
+    public void AddTest()
+    {
+        double result = _calculator.Add(5, 3);
+
+        Assert.AreEqual(8, result);
+    }
+
+    [TestMethod()]
+    public void SubtractTest()
+    {
+        double result = _calculator.Subtract(5, 3);
+
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod()]
+    public void MultiplyTest()
+    {
+        double result = _calculator.Multiply(5, 3);
+
+        Assert.AreEqual(15, result);
+    }
+
+    [TestMethod()]
+    public void DivideTest()
+    {
+        double result = _calculator.Subtract(6, 2);
+
+        Assert.AreEqual(3, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void DivideByZeroTest()
+    {
+        double result = _calculator.Divide(5, 0);
+    }
+}
---
+++ Test.Calculadora.UnitTest.AppxUnit/CalculatorTest.cs
@@ -0,0 +1,59 @@
+﻿namespace Test.Calculadora.UnitTest.AppxUnit;
+
+public class CalculatorTest
+{
+    private readonly Calculator _calculator = new Calculator();
+
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        double num1 = 5.0;
+        double num2 = 3.0;
+
+        double result = _calculator.Add(num1, num2);
+
+        Assert.Equal(8.0, result);
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnCorrectDifference()
+    {
+        double num1 = 8.0;
+        double num2 = 3.0;
+
+        double result = _calculator.Subtract(num1, num2);
+
+        Assert.Equal(5.0, result);
+    }
+
+    [Fact]
+    public void Multiply_ShouldReturnCorrectProduct()
+    {
+        double num1 = 4.0;
+        double num2 = 2.0;
+
+        double result = _calculator.Multiply(num1, num2);
+
+        Assert.Equal(8.0, result);
+    }
+
+    [Fact]
+    public void Divide_ShouldReturnCorrectQuotient()
+    {
+        double num1 = 10.0;
+        double num2 = 2.0;
+
+        double result = _calculator.Divide(num1, num2);
+
+        Assert.Equal(5.0, result);
+    }
+
+    [Fact]
+    public void Divide_ByZero_ShouldThrowException()
+    {
+        double num1 = 5.0;
+        double num2 = 0.0;
+
+        Assert.Throws<ArgumentException>(() => _calculator.Divide(num1, num2));
+    }
+}
